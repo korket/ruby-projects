e3c3f6d1d9026d8dd879ace39cb86182a3d5264c
korket <korket1234@gmail.com>
2023-04-07 10:34:41 +0700

    feat: add guess_check and update_hint

--- hangman/main.rb
+++ hangman/main.rb
@@ -2,9 +2,14 @@
 
 # Game
 class Game
+  attr_accessor :word, :word_array, :hint, :hint_array, :chances
+
   def initialize(dictionary)
     @word = get_word(dictionary)
-    @selection = ''
+    @word_array = word.split('')
+    @hint_array = (String.new('_') * word.length).split('')
+    @hint = hint_array.join(' ')
+    @chances = 7
     introduction
   end
 
@@ -19,11 +24,38 @@ class Game
     words.sample
   end
 
+  def new_game
+    puts "You have #{chances} chances remaining."
+    puts 'Guess the word!'
+    puts ''
+    puts hint
+    puts ''
+    guess = gets.chomp
+    guess_check(word_array, guess)
+  end
+
+  def guess_check(word_array, guess)
+    if word_array.any?(guess)
+      update_hint(guess)
+    else
+      @chances -= 1
+      new_game
+    end
+  end
+
+  def update_hint(guess)
+    word_array.each_with_index do |char, idx|
+      hint_array[idx] = guess if char == guess
+    end
+    @hint = hint_array.join(' ')
+    new_game
+  end
+
   def introduction
     puts 'Welcome to Hangman.'
     puts 'In this game, you have to guess the secret word by'
     puts 'guessing each letter that are in the secret word.'
-    puts "You have seven chances to guess, if you don't get it right,"
+    puts "You have seven chances to guess. If you don't get it right,"
     puts 'the man will be hanged. :('
     puts ''
     select_game
@@ -32,10 +64,16 @@ class Game
   def select_game
     puts "Type '1' to start a new game, or"
     puts "Type '2' to load a saved game."
-    @selection = gets.chomp
-    until @selection == '1' || @selection == '2'
+    selection = gets.chomp
+    until selection == '1' || selection == '2'
       puts 'Please type a valid selection.'
-      @selection = gets.chomp
+      selection = gets.chomp
+    end
+
+    if selection == '1'
+      new_game
+    else
+      puts 'ayaya'
     end
   end
 end
